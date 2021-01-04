20aa638130e08fae78a80d37c30b53c8ffab7292
Fabian Affolter <mail@fabian-affolter.ch>
2021-01-04 22:51:55 +0100

    Add install_requires

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/leofig-rj/pywilight',
+    install_requires = ['requests', 'ifaddr'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
